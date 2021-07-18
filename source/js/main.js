3c849dab169f17b1a60fceb5f0900e2a5f5f105a
askomarov <askomarov13@mail.ru>
2021-07-18 21:30:28 +0300

    добавил вступление

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,7 @@ import { showLinkOnTop, onLinkOnTopClick } from './link-on-top.js';
 import { onHoverBLockFollowCursor, cancelParall } from './parall.js';
 import { createCatalogItems } from './render-catalog.js';
 import { catalogHtml, catalogJs } from './data-catalog.js';
+import { typing } from './typing.js';
 
 const page = document.querySelector(".page");
 const headerMenuToggle = document.querySelector(".header-menu__toggle");
@@ -16,8 +17,29 @@ const promo = document.querySelector('.promo__container');
 const catalogListHtml = document.querySelector('.catalog-list--html');
 const catalogListJs = document.querySelector('.catalog-list--js');
 
+
+const helloBlock = document.querySelector('.hello');
+const welcome = () => {
+  if (helloBlock) {
+    page.classList.add('lock');
+    let typingTextParent = helloBlock.querySelector('.hello__typing-text');
+    let text = helloBlock.textContent;
+    typing(text, typingTextParent, 50)
+
+    setTimeout(() => {
+      helloBlock.classList.add('loaded');
+      page.classList.remove('lock');
+    }, 2500);
+  }
+};
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   page.classList.remove('no-js');
+  
+  welcome();
+
 
   headerMenuToggler(headerMenuToggle, headerMenu);
 
@@ -43,4 +65,6 @@ document.addEventListener("DOMContentLoaded", () => {
     onHoverBLockFollowCursor(promo);
     cancelParall(promo);
   }
+
+
 });
---
+++ source/js/typing.js
@@ -0,0 +1,14 @@
+//эффект печатания
+
+const typing = (text, elem, delay) => {
+  if (text.length > 0) {
+    elem.innerHTML += text[0];
+    setTimeout(
+      () => {
+        typing(text.slice(1), elem, delay);
+      }, delay
+    );
+  }
+};
+
+export { typing };
